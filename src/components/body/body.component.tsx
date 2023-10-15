01d2115f5224b64e91269a1ad746f38da76ac489
destiny-fucker <emaangrg@gmail.com>
2023-10-15 22:41:51 +0545

    changed mobile view and css

--- src/components/body/body.component.tsx
+++ src/components/body/body.component.tsx
@@ -4,43 +4,23 @@ import { Grid } from '@mantine/core';
 
 import { useMediaQuery } from 'react-responsive'
 import "./body.styles.scss";
-import ContentHeader from "../content-header/content-header.component";
-
-import StoreButton, {BUTTON_TYPES} from "../store button/store-button.component";
 
 
 
 const Body = () => {
     
-    //useMediaQuery is only used here for component rearragenment for mobile view
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1000px)' })
+    //useMediaQuery is only used here and in header.component.tsx for mobile view
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1023px)' })
     const col = isTabletOrMobile? 12: 5;
 
     return (
         <Grid align="center" justify="center" className="body" >
-            {
-                isTabletOrMobile? (
-                    <div className="body-content">
-                        <ContentHeader />
-                        <Grid.Col span={col} className="trip-img-container">
-                            <img src= {Trips} className="trips" alt="trips" />
-                        </Grid.Col>
-                        <Grid.Col span={col} className="mobile-footer">
-                            <StoreButton buttonType= {BUTTON_TYPES.google} />
-                            <StoreButton buttonType= {BUTTON_TYPES.apple} />
-                        </Grid.Col>
-                    </div>
-
-                ): (
-                    <>
-                        <Content col={col} />
-                        <Grid.Col span={col}>
-                            <img src= {Trips} className="trips" alt="trips" />
-                        </Grid.Col>
-                    </>
-                )
-            }
-           
+            <div className="body-content">
+                <Content col={col} />
+                <Grid.Col span={col} className="trips-img-container">
+                    <img src= {Trips} className="trips" alt="trips" />
+                </Grid.Col>
+            </div>
         </Grid>
     )
 }
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,15 +1,25 @@
+import { useMediaQuery } from 'react-responsive'
+
 import BaccpaccModal from "../baccpacc-modal/baccpacc-modal";
 import Logo from "../logo/logo.component";
+
 import "./header.styles.scss";
 
 const Header = () => {
+
+    //useMediaQuery is only used here and in body.component.tsx for mobile view
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1023px)' })
+    
+
     return (
         <div className="header">
             <div className="logo-container">
                 <Logo />
                 <p>baccpacc</p>
             </div>
-            <BaccpaccModal />
+            {
+                !isTabletOrMobile && <BaccpaccModal />
+            }
         </div>
     )
 }
