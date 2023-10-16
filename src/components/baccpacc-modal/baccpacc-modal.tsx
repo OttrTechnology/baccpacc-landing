c6fe33480938d395b600c991e3614e7a7131ecf9
destiny-fucker <emaangrg@gmail.com>
2023-10-16 13:04:41 +0545

    changed portal location

--- src/components/baccpacc-modal/baccpacc-modal.tsx
+++ src/components/baccpacc-modal/baccpacc-modal.tsx
@@ -7,26 +7,31 @@ import "./baccpacc-modal.styles.scss";
 
 const BaccpaccModal = () => {
   const [opened, { open, close }] = useDisclosure(false);
+  const location = document.getElementById("modal");
+
 
   return (
     <>
-      {createPortal(
-        <Modal
-          opened={opened}
-          onClose={close}
-          padding={0}
-          centered
-          size="auto"
-          className="modal"
-          radius={"1.25rem"}
-        >
-          <div className="modal-content">
-            <img src={Qr} alt="QR Code" className="qr-container" />
-            <p className="modal-text">Scan to download Baccpacc</p>
-          </div>
-        </Modal>,
-        document.body
-      )}
+      {
+         location &&
+           createPortal(
+             <Modal
+               opened={opened}
+               onClose={close}
+               padding={0}
+               centered
+               size="auto"
+               className="modal"
+               radius={"1.25rem"}
+             >
+               <div className="modal-content">
+                 <img src={Qr} alt="QR Code" className="qr-container" />
+                 <p className="modal-text">Scan to download Baccpacc</p>
+               </div>
+             </Modal>,
+             location
+           )
+    }
 
       <button onClick={open} className="get-baccpacc">
         Get Baccpacc
