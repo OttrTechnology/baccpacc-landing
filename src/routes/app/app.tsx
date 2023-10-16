15f1e297c0ca9c1324ef8e6c794259865d980033
destiny-fucker <emaangrg@gmail.com>
2023-10-16 11:33:16 +0545

    switched to pnpm and also changed component in /app to use react-device-detect
    Closes #1

--- src/routes/app/app.tsx
+++ src/routes/app/app.tsx
@@ -1,22 +1,20 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { isIOS, isAndroid } from 'react-device-detect';
 
 const MobileApp = () => {
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        const userAgent = navigator.userAgent.toLowerCase();
-    
-        if (userAgent.indexOf('iphone') !== -1 || userAgent.indexOf('ipad') !== -1) {
-          // Redirect iOS devices to one URL
-          window.location.href = 'https://apps.apple.com/np/app/baccpacc/id6448676201';
-        } else if (userAgent.indexOf('android') !== -1) {
-          // Redirect Android devices to another URL
-          window.location.href = 'https://play.google.com/store/apps/details?id=com.ottr.baccpacc&hl=en&gl=US';
-        } else {
-            navigate("/");
+          if (isIOS) {
+            window.location.href = 'https://apps.apple.com/np/app/baccpacc/id6448676201';
+          } else if (isAndroid) {
+            window.location.href = 'https://play.google.com/store/apps/details?id=com.ottr.baccpacc&hl=en&gl=US';
+          } else {
+          navigate("/");
         }
+
       }, []);
 
       return "";
