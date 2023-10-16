22d69f6ea46051566d7c1072af3f270c89b99b55
rubek <rubek@ottr.com.np>
2023-10-16 15:13:29 +0545

    🎨 Style: prettify

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,16 +1,15 @@
+import { Container } from "@mantine/core";
 import Header from "../../components/header/header.component";
 import Body from "../../components/body/body.component";
-import { Container } from '@mantine/core';
-
 import "./home.styles.scss";
 
 const Home = () => {
-    return (
-        <Container size={"91.66667%"} className="home">
-            <Header />
-            <Body />
-        </Container>
-    )
-}
+  return (
+    <Container size={"91.66667%"} className="home">
+      <Header />
+      <Body />
+    </Container>
+  );
+};
 
 export default Home;
