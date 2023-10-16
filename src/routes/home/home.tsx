6e4fd450108d329f4e177278ab558cfdb5199848
destiny-fucker <emaangrg@gmail.com>
2023-10-16 14:31:30 +0545

    updated to use mantine container and decreades header, body top padding

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,15 +1,16 @@
 import Header from "../../components/header/header.component";
 import Body from "../../components/body/body.component";
+import { Container } from '@mantine/core';
 
 import "./home.styles.scss";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <Header />
-      <Body />
-    </div>
-  );
-};
+    return (
+        <Container size={"91.66667%"} className="home">
+            <Header />
+            <Body />
+        </Container>
+    )
+}
 
 export default Home;
